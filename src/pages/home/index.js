f7dfd9b962862e31dcb0c990a8fb35c06a463955
Filipe Biasoli <filipe.biasoli@gmail.com>
2023-03-29 11:17:56 -0300

    login

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./style.scss";
+import api from "../../services/api";
 
 const Home = () => {
-  return <h1>Oi mundo</h1>;
+  async function userLogin(e) {
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    await api
+      .get(`/user/get_email/${email}`)
+      .then((res) => {
+        if (res.data.password != password || res.data == null) {
+          document.getElementById("error_login").style.display = "block";
+        } else {
+          console.log("senha certa");
+        }
+      })
+      .catch((err) => {
+        document.getElementById("error_login").style.display = "block";
+      });
+  }
+
+  return (
+    <>
+      <form onSubmit={userLogin}>
+        <input type="email" placeholder="E-mail" id="email" required />
+        <input type="password" placeholder="Senha" id="password" />
+        <button type="submit">Login</button>
+      </form>
+
+      <div id="error_login">Usuário ou senha incorretos, tente novamente</div>
+    </>
+  );
 };
 
 export default Home;
